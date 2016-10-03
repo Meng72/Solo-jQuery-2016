854ca52a807f1b6003c3101ac652ba017e435d0c
Meng72 <matthewkeng@hotmail.com>
2016-10-03 15:49:42 -0500

    work on challenge 2

---
+++ app.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+//counters = 0
+var redCount = 0;
+var yellowCount = 0;
+var greenCount = 0;
+var blueCount = 0;
+
+//event listener
+$('.color-button').on('click', function() {
+
+  $('.container').append('<div class="color-cube"></div>');
+  //function that assigns color class to div based on button clicked
+  //function that increases button click count
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
+
+
+
+
+
+//end of ready function
+
+//function to add <div> class="color-cube"
