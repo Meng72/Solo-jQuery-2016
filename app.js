28339132bb0c0c06b97e39ac217913c96c88240c
Meng72 <matthewkeng@hotmail.com>
2016-10-03 19:56:29 -0500

    complete code challenge 2

--- app.js
+++ app.js
@@ -7,24 +7,28 @@ var blueCount = 0;
 
 //event listener
 $('.color-button').on('click', function() {
+  if ($(this).data('color') === 'red') {
+    $('.container').append('<div class="red color-cube"></div>');
+    redCount++;
+    $('#red').text('Total red: ' + redCount);
 
-  $('.container').append('<div class="color-cube"></div>');
-  //function that assigns color class to div based on button clicked
-  //function that increases button click count
-})
+  }else if ($(this).data('color') === 'yellow'){
+    $('.container').append('<div class="yellow color-cube"></div>');
+    yellowCount++;
+    $('#yellow').text('Total yellow: ' + yellowCount);
 
+  }else if ($(this).data('color') === 'green'){
+    $('.container').append('<div class="green color-cube"></div>');
+    greenCount++;
+    $('#green').text('Total green: ' + greenCount);
 
+  }else{
+    $('.container').append('<div class="blue color-cube"></div>');
+    blueCount++;
+    $('#blue').text('Total blue: ' + blueCount);
+  }
 
-
-
-
-
-
-
-
-
-
-
+  })
 })
 
 
